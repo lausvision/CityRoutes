90312ccce152a9ac2a6f2d84a1e50dcd20fc8ed3
laura <lauramolinalopez02@gmail.com>
2020-09-06 12:20:39 +0200

    no map transfer frontend

--- app/screens/Transfer.js
+++ app/screens/Transfer.js
@@ -6,65 +6,77 @@ import {
     TouchableOpacity,
     Image,
     ScrollView,
+    Linking
 } from 'react-native';
 
 
 export default class Transfer extends React.Component {
 
 
-    render(){
+    render() {
         return (
             <View style={styles.container}>
-               
-               
-               <ScrollView style={styles.scrollConatainer}>
 
-               <View style={styles.pricebuttonContainer}>
+
+                <ScrollView style={styles.scrollConatainer}>
+
+                    <View style={styles.pricebuttonContainer}>
                         <TouchableOpacity style={styles.priceButton}>
                             <Text style={styles.priceButtonText}>OTHER OPTIONS</Text>
                         </TouchableOpacity>
-                        </View>
+                    </View>
 
-               <View style={styles.imageContainer}>
+                    <View style={styles.imageContainer}>
                         <Image
                             source={require('./img/taxi.png')}
                             style={styles.image}
                         />
-                        <View>
-                        <Text>taxi</Text>
-                        <Text>price from 5€ to 50€</Text>
+                        <View style={styles.textContainer}>
+                            <Text>taxi</Text>
+                            <Text>price from 5€ to 50€</Text>
                         </View>
                     </View>
 
-                    
-               <View style={styles.imageContainer}>
+                    <View
+                        style={{
+                            borderBottomColor: 'black',
+                            borderBottomWidth: 1,
+                            margin: 10
+                        }}
+                    />
+
+
+                    <View style={styles.imageContainer}>
                         <Image
                             source={require('./img/uber.png')}
                             style={styles.image}
                         />
-                        <View>
-                        <Text>Uber</Text>
-                        <Text>price from 5€ to 50€</Text>
-                        <Text>Call 15199</Text>
-                        <Text>Request a ride</Text>
-                        <Text>https://play.google.com/store/apps/details?id=com.ubercab&hl=en</Text>
+                        <View style={styles.textContainer}>
+
+                            <Text>Uber</Text>
+                            <Text>price from 5€ to 50€</Text>
+                            <Text>Call 15199</Text>
+                            <Text style={{ color: 'blue' }}
+                                onPress={() => Linking.openURL('https://play.google.com/store/apps/details?id=com.ubercab&hl=en')}
+                            >Request a ride</Text>
+
                         </View>
-                        
+
                     </View>
 
-               </ScrollView>
-               
-   
+                </ScrollView>
+
+
             </View>
 
-           
+
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1
+        flex: 1
     },
     scrollConatainer: {
         flex: 1,
@@ -72,12 +84,17 @@ const styles = StyleSheet.create({
     imageContainer: {
         padding: 20,
         flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     image: {
         margin: 0,
         height: 60,
         width: 60,
         resizeMode: 'stretch',
+    },
+    textContainer: {
+        paddingLeft: 35
     },
     pricebuttonContainer: {
         alignItems: 'center',
@@ -91,7 +108,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'black',
         width: 250,
         height: 30,
-        borderWidth: 2.5, 
+        borderWidth: 2.5,
         borderRadius: 13,
         borderColor: 'black',
         alignItems: 'center',
