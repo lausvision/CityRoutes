c0758b031847db38ed9d0dbc4950f1831074f3b8
laura <lauramolinalopez02@gmail.com>
2020-09-07 12:08:34 +0200

    map directions

--- app/components/DirectionsMap.js
+++ app/components/DirectionsMap.js
@@ -19,20 +19,82 @@ import MapView from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import getDirections from 'react-native-google-maps-directions';
 
+let Container = styled(View)`
+	width: 100%;
+	height: 100%;
+	background-color: white;
+`;
+
+let Map = styled(MapView)`
+	width: 100%;
+	height: 100%;
+`;
+
+const origin = { latitude: 42.2678176, longitude: -71.000124 };
+const destination = { latitude: 42.2929175, longitude: -71.0548235 };
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCCExFZgGsyosGTJ5e_GjshKczB-7OgnEc';
+
 
 export default class Transfer extends React.Component {
+
 
     constructor() {
         super();
     }
 
+    handleGetDirections = () => {
+        const data = {
+            destination,
+            params: [
+                {
+                    key: "dirflg",
+                    value: "d"
+                }
+            ]
+        }
 
+        getDirections(data)
+    }
+
+    pressed(e) {
+        console.log('pressed');
+
+    }
     render() {
         return (
             <View style={styles.container}>
 
 
                 <ScrollView style={styles.scrollConatainer}>
+
+
+                    <View>
+                        <Map
+                            initialRegion={{
+                                latitude: 42.2678176,
+                                longitude: -71.000124,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        >
+                            <MapView.Marker
+                                coordinate={destination}
+                            >
+                                <MapView.Callout onPress={() => {
+                                    console.log('Press to Get Direction');
+                                    this.handleGetDirections();
+                                }}>
+                                    <Text>Press to Get Direction</Text>
+                                </MapView.Callout>
+                            </MapView.Marker>
+                            <MapViewDirections
+                                origin={origin}
+                                destination={destination}
+                                apikey={GOOGLE_MAPS_APIKEY}
+                            />
+
+                        </Map>
+                    </View>
 
                     <View style={styles.pricebuttonContainer}>
                         <TouchableOpacity style={styles.priceButton}>
