ca3208b4b682bb827ecc49fe1019eba30a478ea3
Niels <61013531+nielshub@users.noreply.github.com>
2020-09-14 23:24:06 +0200

    add structure for redux

--- app/api.js
+++ app/api.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const { BASE_URL } = require("./config");
+//const { BASE_URL } = require("./config");
+const BASE_URL = "http://localhost:8080";
 
 const _apiCall = async (method, path, payload) => {
   let options = { method, mode: "cors" };
@@ -15,8 +16,8 @@ const _apiCall = async (method, path, payload) => {
 export default {
   loadCountries: () => _apiCall("GET", "/countries"),
   loadCities: () => _apiCall("GET", "/cities"),
-  loadCitiesfromCountry: () => _apiCall("GET", `/cities/${country.id}`),
+  loadCitiesfromCountry: (country) => _apiCall("GET", `/cities/${country.id}`),
   loadTypologies: () => _apiCall("GET", "/typologies"),
-  loadActivities: () => _apiCall("GET", "/places", payload),
-  loadRoutes: () => _apiCall("GET", "/routes", payload),
+  loadActivities: (payload) => _apiCall("GET", "/places", payload),
+  loadRoutes: (payload) => _apiCall("GET", "/routes", payload),
 };
