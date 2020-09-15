1f4512f440ac1abe6aad86ebd731638a606118b9
lgotarra <97gotarra@gmail.com>
2020-09-15 11:20:43 +0200

    Combined reducers and created store.

--- app/store.js
+++ app/store.js
@@ -1,11 +1,19 @@
-import { applyMiddleware, createStore, compose } from "redux";
+import { applyMiddleware, createStore, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import todosReducer from "./reducers/todos";
+import {activitiesReducer, citiesReducer, countriesReducer, typologiesReducer, routesReducer} from "./reducers/index";
 import api from './api';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const todosStore = createStore(
-  todosReducer,
+const rootReducer = combineReducers ({
+  activitiesReducer,
+  citiesReducer,
+  countriesReducer,
+  typologiesReducer,
+  routesReducer
+})
+
+export const store = createStore(
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk.withExtraArgument(api)))
 );
