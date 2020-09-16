87461f05fb9f5cb0322ae4c7a5b0c3a11f3b693e
lgotarra <97gotarra@gmail.com>
2020-09-16 09:56:06 +0200

    Added custom states types to manage better the dispatch process for multiple reducers for one store

--- app/actions/routes.js
+++ app/actions/routes.js
@@ -2,11 +2,11 @@
 import types from './types';
 
 export const loadRoutes = () => async (dispatch, getState , api) => {
-  dispatch({ type: types.LOAD_STARTED });
+  dispatch({ type: types.LOAD_STARTED_ROUTES });
   try {
     let selectedRoute = await api.loadRoutes(getState.activitiesReducer);
-    dispatch({ type: types.LOAD_SUCCEEDED, payload: selectedRoute });
+    dispatch({ type: types.LOAD_SUCCEEDED_ROUTES, payload: selectedRoute });
   } catch (error) {
-    dispatch({ type: types.LOAD_FAILED, payload: error });
+    dispatch({ type: types.LOAD_FAILED_ROUTES, payload: error });
   }
 }
